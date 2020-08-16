ae464128cbc5a83375a34ef1d358a191501068e8
eganpido <eganpido90@gmail.com>
2020-08-16 16:02:07 +0800

    Add project files.

---
+++ TestProject/ClientInformation.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="ClientInformation.aspx.vb" Inherits="TestProject.ClientInformation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle4 {
+            border-style: solid;
+        }
+    </style>
+    <h1>TECHNICAL EXAM</h1>
+</head>
+<body>
+    <form id="form1" runat="server">  
+        <asp:Label ID="Label1" runat="server" Text="Name :"></asp:Label>
+        <asp:TextBox ID="txtName" runat="server" style="margin-left: 63px" Width="207px" CssClass="newStyle4"></asp:TextBox>
+        <br>
+        <br>
+        <asp:Label ID="Label2" runat="server" Text="Phone Number :"></asp:Label>
+        <asp:TextBox ID="txtPhoneNumber" runat="server" style="margin-left: 6px" Width="207px" CssClass="newStyle4"></asp:TextBox>
+        <asp:FileUpload ID="FileUpload1" runat="server" style="margin-left: 6px" Width="295px" BorderStyle="Ridge" />
+        <asp:Button ID="Button3" runat="server" Text="Import CSV" style="margin-left: 6px" Width="115px" CssClass="newStyle4" Height="22px"/>
+        <br>
+        <br>
+        <asp:Label ID="Label3" runat="server" Text="City :"></asp:Label>
+        <asp:TextBox ID="txtCity" runat="server" style="margin-left: 74px; margin-top: 0px;" Width="207px" CssClass="newStyle4"></asp:TextBox>
+        <br>
+        <br>
+        <asp:Label ID="Label4" runat="server" Text="Date :"></asp:Label>
+        <asp:TextBox ID="txtDate" runat="server" style="margin-left: 71px" Width="207px" CssClass="newStyle4"></asp:TextBox>
+        <br>
+        <br>
+        <asp:Button ID="Button1" runat="server" Text="View Data" Height="33px" style="text-align: center; margin-left: 3px; margin-top: 20px" Width="257px" />
+        <asp:Button ID="Button2" runat="server" Text="Insert Data" Height="33px" style="text-align: center; margin-left: 3px; margin-top: 20px" Width="257px" />
+        <asp:GridView ID="grdViewClient" runat="server" Height="229px" style="text-align: center; margin-top: 23px" Width="906px">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ TestProject/ClientInformation.aspx.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Data.SqlClient.SqlException
+Imports System.Configuration
+Imports System.IO
+Imports System.Web
+Public Class ClientInformation
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+
+        con.ConnectionString = "Data Source=EGAN-PC;Initial Catalog=TechExam;Integrated Security=True"
+        con.Open()
+
+        cmd.CommandText = "Select * from Client"
+        cmd.Connection = con
+
+        grdViewClient.DataSource = cmd.ExecuteReader
+        grdViewClient.DataBind()
+
+        con.Close()
+    End Sub
+
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+
+        con.ConnectionString = "Data Source=EGAN-PC;Initial Catalog=TechExam;Integrated Security=True"
+        con.Open()
+
+        cmd = New SqlCommand("INSERT INTO Client VALUES('" & txtName.Text & "','" & txtPhoneNumber.Text & "','" & txtCity.Text & "','" & txtDate.Text & "')", con)
+
+        If (txtName.Text = "" Or txtPhoneNumber.Text = "" Or txtCity.Text = "" Or txtDate.Text = "") Then
+            MsgBox("Please enter all fields!", MsgBoxStyle.Information, "Client Information")
+        Else
+            cmd.ExecuteNonQuery()
+            txtName.Text = ""
+            txtPhoneNumber.Text = ""
+            txtCity.Text = ""
+            txtDate.Text = ""
+            con.Close()
+
+            Button1_Click(sender, e)
+
+            MsgBox("Inserted Sucessfully", MsgBoxStyle.Information, "Client Information")
+        End If
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Upload and save the file.
+        Dim csvPath As String = Server.MapPath("~/Files/Upload/") + Path.GetFileName(FileUpload1.PostedFile.FileName)
+        FileUpload1.SaveAs(csvPath)
+
+        Dim dt As New DataTable()
+        dt.Columns.AddRange(New DataColumn(3) {New DataColumn("Name", GetType(String)), New DataColumn("PhoneNumber", GetType(String)), New DataColumn("City", GetType(String)), New DataColumn("Date", GetType(Date))})
+
+        Dim skip As Boolean = True
+        Dim csvData As String = File.ReadAllText(csvPath)
+        For Each row As String In csvData.Split(ControlChars.Lf)
+            If skip = True Then
+                skip = False
+            Else
+                If Not String.IsNullOrEmpty(row) Then
+                    dt.Rows.Add()
+                    Dim i As Integer = 0
+                    For Each cell As String In row.Split(",")
+                        dt.Rows(dt.Rows.Count - 1)(i) = cell
+                        i += 1
+                    Next
+                End If
+            End If
+        Next
+
+
+
+        Dim consString As String = "Data Source=EGAN-PC;Initial Catalog=TechExam;Integrated Security=True"
+        Using con As New SqlConnection(consString)
+            Using sqlBulkCopy As New SqlBulkCopy(con)
+                'Set the database table name.
+                sqlBulkCopy.DestinationTableName = "dbo.Client"
+                con.Open()
+                sqlBulkCopy.WriteToServer(dt)
+                con.Close()
+            End Using
+        End Using
+
+        Button1_Click(sender, e)
+
+        MsgBox("Imported Successfully!", MsgBoxStyle.Information, "Import CSV File")
+    End Sub
+End Class
